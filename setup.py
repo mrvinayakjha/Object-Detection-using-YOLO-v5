86b8ec023ea12f14312fd4911865f13216706086
VINAYAK <vinayak@192.168.1.5>
2023-11-17 07:09:01 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'objectDetection',
+    version= '0.0.0',
+    author= 'Vinayak Jha',
+    author_email= 'vinayakkumarjha2000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
